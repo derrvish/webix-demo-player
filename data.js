66ae9c4769fa4dc748101d60d28e305e279d635d
Stanislav <derrvish@monoxrom.net>
2015-04-23 18:55:27 +0300

    base app

---
+++ data.js
@@ -0,0 +1,47 @@
+recordsData = [  
+{id:"1", value:"Oceansize", data: [
+    {id:"oceanData", value:"Everyone Into Position"},
+    ]},
+{id:"2", value:"Little People", data: [
+    {id:"peopleData", value:"Mickey Mouse Operation"},
+    ]},
+];
+
+gridColumns = [
+    {
+        dataIndex: "title",
+        header: "Title"
+    },
+    {
+        dataIndex: "duration",
+        header: "Duration"
+    }
+];
+
+oceanData = [
+    {id: "1", title: "01. The Charm Offensive", duration:"7:19"},
+    {id: "2", title: "02. Heaven Alive", duration:"6:20"},
+    {id: "3", title: "03. A Homage to Shame", duration:"5:52"},
+    {id: "4", title: "04. Meredith", duration:"5:26"},
+    {id: "5", title: "05. Music for a Nurse", duration:"8:16"},
+    {id: "6", title: "06. New Pin", duration:"5:11"},
+    {id: "7", title: "07. No Tomorrow", duration:"7:10"},
+    {id: "8", title: "08. Mine Host", duration:"4:10"},
+    {id: "9", title: "09. You Can't Keep a Bad Man Down", duration:"7:36"},
+    {id: "10", title: "10. Ornament. The Last Wrongs", duration:"9:21"},
+];
+
+peopleData = [
+    {id: "1", title: "01. Basique", duration: "3:38"},
+    {id: "2", title: "02. Moon", duration: "3:47"},
+    {id: "3", title: "03. Unsaid", duration: "3:48"},
+    {id: "4", title: "04. Eitheror", duration: "5:45"},
+    {id: "5", title: "05. Above the Clouds", duration: "3:50"},
+    {id: "6", title: "06. Breathe Again (feat. Rachael Roberts)", duration: "4:01"},
+    {id: "7", title: "07. Intermezzo", duration: "0:37"},
+    {id: "8", title: "08. Idiom", duration: "5:45"},
+    {id: "9", title: "09. Inutile et Indispensable", duration: "4:58"},
+    {id: "10", title: "10. Last Fare", duration: "2:45"},
+    {id: "11", title: "11. Fisticuffs at Dawn", duration: "1:02"},
+    {id: "12", title: "12. Gravitas", duration: "5:10"},
+]
